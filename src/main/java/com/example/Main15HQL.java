5d9bcba635bdd1c0a4d03d5bfe5214e04600bc74
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-08-01 15:36:37 +0200

    query.getResultList

--- src/main/java/com/example/Main15HQL.java
+++ src/main/java/com/example/Main15HQL.java
@@ -1,5 +1,35 @@
 package com.example;
 
+import com.example.entity.Husband;
+import com.example.entity.School;
+import com.example.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
 public class Main15HQL {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query firstQuery = session.createQuery("FROM Husband");
+        List<Husband> resultList = firstQuery.getResultList();
+        resultList.stream().map(Husband::getName).forEach(System.out::println);
+
+        Query secondQuery = session.createQuery("SELECT h.name FROM Husband AS h");
+        List<String> resultList2 = secondQuery.getResultList();
+        resultList2.stream().forEach(System.out::println);
+
+
+        transaction.commit();
+        session.close();
+    }
 
 }
